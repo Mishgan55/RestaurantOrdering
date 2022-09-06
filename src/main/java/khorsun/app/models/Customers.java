b5c615c38b8f4af828313f40d803974246a021d4
mishgan55 <vorota.mogilev@gmail.com>
2022-09-07 01:57:07 +0300

    add Customers

---
+++ src/main/java/khorsun/app/models/Customers.java
@@ -0,0 +1,25 @@
+package khorsun.app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customers {
+
+    private int id;
+    private  String name;
+    private  String email;
+    private String phoneNumber;
+    private Timestamp creationDate;
+    private Timestamp modificationDate;
+
+
+
+
+}
